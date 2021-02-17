4e4e830b89db8544e44a08247a230f5423b6882c
99003756 <78857426+99003756@users.noreply.github.com>
2021-02-17 15:58:05 +0530

    Add files via upload

---
+++ Implementation/inc/check.h
@@ -0,0 +1,31 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char s1[100];
+int l;
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 + operand2
+*/
+char* breaker(char *str);
+
+/**
+*  subtracts the operand1, operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of operand1 - operand2
+*/
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
